88ffcaecdd2141aab64dc0ae638f3d1feacb1fad
Dave Williams <mail@d4v3w.com>
2021-05-16 17:03:39 +0100

    Upgrade config to better support webpack 5 babel and storybook builds

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,12 @@
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-postcss"
+    "@storybook/addon-postcss",
   ],
   core: {
     builder: "webpack5",
