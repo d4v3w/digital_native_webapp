71a33dae78609058125a42742b9cbd539a2d1f03
Dave Williams <mail@d4v3w.com>
2021-06-11 18:37:03 +0100

    Update index to call Hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 import Markdown from '../components/Markdown'
+import Hero from '../components/Hero'
 import styles from '../components/layout.module.css'
 
 const IndexPage = (): JSX.Element => (
   <Layout title='Home'>
+    <Hero content="Digital Native" />
     <article className={styles.article}>
       <h1 className={styles.heading2Xl}>Welcome to Digital Native Online 👋</h1>
       <section className={styles.section}>
