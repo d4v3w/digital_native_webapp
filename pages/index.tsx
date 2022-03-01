fcec4599900923b9b64715000d2db462a77870ad
Dave Williams <dave@fragile-media.co.uk>
2022-03-01 20:21:15 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Layout from '../components/Layout'
 import Hero from '../components/Hero'
-import styles from '../components/layout.module.css'
 
 const IndexPage = (): JSX.Element => (
   <Layout title='Home'>
