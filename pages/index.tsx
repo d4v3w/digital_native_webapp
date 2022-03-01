c0e63a2148d5834218d0dcce308b556fde72072d
Dave Williams <dave@fragile-media.co.uk>
2022-03-01 20:14:10 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,18 +7,6 @@ import styles from '../components/layout.module.css'
 const IndexPage = (): JSX.Element => (
   <Layout title='Home'>
     <Hero content="Digital Native" />
-    <article className={styles.article}>
-      <h1 className={styles.heading2Xl}>Welcome to Digital Native Online 👋</h1>
-      <section className={styles.section}>
-        <Markdown content="Welcome to Digital Native Online:" />
-        <Link href='/about' >
-          <a>Find out more about Digital Native here.</a>
-        </Link>
-      </section>
-      <section className={styles.section}>
-        <Markdown content="Happy Jungling!" />
-      </section>
-    </article>
   </Layout>
 )
 
