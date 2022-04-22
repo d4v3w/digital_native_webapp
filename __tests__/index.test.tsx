79fc81762fdd3bb1a753895390a2297f149dad19
Dave Williams <mail@d4v3w.com>
2022-04-22 15:02:33 +0100

    Add jest to project

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import Layout from '../components/Layout'
+import '@testing-library/jest-dom'
+
+// describe('Layout', () => {
+//   it('renders a heading', () => {
+//     render(<Layout />)
+
+//     const heading = screen.getByRole('heading', {
+//       name: /welcome to next\.js!/i,
+//     })
+
+//     expect(heading).toBeInTheDocument()
+//   })
+// })
---
+++ __tests__/snapshot.tsx
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react'
+import IndexPage from '../pages/index'
+
+// it('renders homepage unchanged', () => {
+//   const { container } = render(<IndexPage />)
+//   expect(container).toMatchSnapshot()
+// })
