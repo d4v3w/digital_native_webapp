6f9121feb11b7e509c93a2417011b8517ff05741
Dave Williams <dave@fragile-media.co.uk>
2022-06-02 13:35:37 +0100

    Update production.tsx

--- pages/production.tsx
+++ pages/production.tsx
@@ -1,4 +1,3 @@
-
 import Layout from '../components/Layout'
 import styles from '../components/layout.module.css'
 import Link from 'next/link'
@@ -11,27 +10,6 @@ const ProductionPage = () => (
       <p>
         With a sound on the deeper, liquid side of dnb, but not averse to more technical, jungly or darker styles and
         drawing influence from a broad musical taste, the focus is on creating a musical journey.
-      </p>
-      <p>
-        Expect old breaks, musicality, emotion and soul at the core of anything you hear from Digital Native with
-        attention to the details.
-      </p>
-      <p>
-        Releases on Goldfat, Hospital, Glitch Audio, Beats In Mind, Silent Audio, Liquid Brilliants and SIMBAL
-        Recordings. Self releases available to stream or download via Bandcamp and Soundcloud.
-      </p>
-      <p>
-        <Link href="https://digitalnativeuk.bandcamp.com" passHref>
-          <a>Bandcamp: digitalnativeuk</a>
-        </Link>
-        <br />
-        <Link href="https://soundcloud.com/digitalnativeuk" passHref>
-          <a>Soundcloud: digitalnativeuk</a>
-        </Link>
-        <br />
-        <Link href="https://www.facebook.com/digitalnativeuk" passHref>
-          <a>Facebook: digitalnativeuk</a>
-        </Link>
       </p>
     </article>
   </Layout>
