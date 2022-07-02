5145810096ae485b819abc7bf06921040b6f673c
Dave Williams <mail@d4v3w.com>
2022-07-02 16:14:43 +0100

    move public images to avoid static cloudfront path

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,13 @@ import Layout from '../components/Layout'
 import Image from 'next/image'
 import Hero from '../components/Hero'
 import styles from '../components/layout.module.css'
+import heroImage from '../public/images/hero/digital_native_profile_portrait.jpg'
 
 const IndexPage = (): JSX.Element => (
   <Layout title="Home" className="flex">
     <article className={styles.article}>
       <Image
-        src="/static/images/hero/digital_native_profile_portrait.jpg"
+        src={heroImage}
         alt="Digital Native: Drum and Bass Producer"
         width={512}
         height={512}
