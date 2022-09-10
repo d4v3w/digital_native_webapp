f937cd65ebbc4c17d1163dfae69a7d2d82fcb841
Dave Williams <mail@d4v3w.com>
2022-09-10 12:39:15 +0100

    Update layout hero styles and widths

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,30 +15,25 @@ export interface HeroProps {
  */
 export const Hero: React.FC<HeroProps> = ({ className }) => {
   return (
-    <article className={classNames(styles.article, styles.hero, className)}>
-      <Image
-        src={heroImage}
-        alt="Digital Native: Drum and Bass Producer"
-        width={512}
-        height={512}
-        layout={'responsive'}
-        priority={true}
-      />
-      <h1 className={styles.headingPrimary}>Digital Native UK</h1>
-      <p>
-        Releases on Goldfat, Hospital, Glitch Audio, Beats In Mind, Silent Audio, Liquid Brilliants and SIMBAL
-        Recordings. Self releases available to stream or download via Bandcamp and Soundcloud.
-      </p>
-      <div>
-        <div>
-          <iframe
-            src="https://open.spotify.com/embed/playlist/5Ohs23L7Llqp2Z0ujz4i3O?utm_source=generator&theme=0"
-            width="100%"
-            height="80"
-            frameBorder="0"
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-          ></iframe>
-        </div>
+    <article className={classNames(styles.hero, className)}>
+      <div className={styles.heroImage}>
+        <Image
+          src={heroImage}
+          alt="Digital Native: Drum and Bass Producer"
+          width={512}
+          height={512}
+          layout={'responsive'}
+          priority={true}
+        />
+      </div>
+      <div className={styles.heroPlayer}>
+        <iframe
+          src="https://open.spotify.com/embed/playlist/5Ohs23L7Llqp2Z0ujz4i3O?utm_source=generator&theme=0"
+          width="100%"
+          height="80"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        ></iframe>
       </div>
     </article>
   )
