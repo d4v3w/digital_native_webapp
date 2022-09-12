297c886e00be511abf32ad70458467f990d579e8
Dave Williams <mail@d4v3w.com>
2022-09-12 23:14:15 +0100

    fix prettier

--- pages/production.tsx
+++ pages/production.tsx
@@ -7,41 +7,30 @@ const ProductionPage = (): JSX.Element => (
     <main>
       <article className={styles.article}>
         <h1 className={styles.headingPrimary}>Digital Native Production</h1>
-        <p>In this section youll find tutorials, samples and resources that will hopefully help you in your own production journey. </p>
         <p>
-          Check out the links below to navigate quickly to section you need.
+          In this section youll find tutorials, samples and resources that will hopefully help you in your own
+          production journey.{' '}
         </p>
+        <p>Check out the links below to navigate quickly to section you need.</p>
       </article>
       <section>
         <article className={styles.article}>
           <div className={styles.player}>
-            <ReactPlayer
-              url="https://www.youtube.com/watch?v=KIdN4IszoVE&t"
-              height={'100%'}
-              width={'100%'}
-            />
+            <ReactPlayer url="https://www.youtube.com/watch?v=KIdN4IszoVE&t" height={'100%'} width={'100%'} />
           </div>
         </article>
       </section>
       <section>
         <article className={styles.article}>
           <div className={styles.player}>
-            <ReactPlayer
-              url="https://www.youtube.com/watch?v=vp1Z3z59Mlk"
-              height={'100%'}
-              width={'100%'}
-            />
+            <ReactPlayer url="https://www.youtube.com/watch?v=vp1Z3z59Mlk" height={'100%'} width={'100%'} />
           </div>
         </article>
       </section>
       <section>
         <article className={styles.article}>
           <div className={styles.player}>
-            <ReactPlayer
-              url="https://www.youtube.com/watch?v=3SwZjXyKlbg&t"
-              height={'100%'}
-              width={'100%'}
-            />
+            <ReactPlayer url="https://www.youtube.com/watch?v=3SwZjXyKlbg&t" height={'100%'} width={'100%'} />
           </div>
         </article>
       </section>
