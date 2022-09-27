2d276e3f072677231b23574dfcc1a0a61d0265b7
Dave Williams <mail@d4v3w.com>
2022-09-27 18:29:41 +0100

    Head sticky on scroll up

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { Nav } from './Nav'
 import classNames from 'classnames'
@@ -10,8 +11,37 @@ export interface HeaderProps {
 }
 
 export const Header: React.FC<HeaderProps> = ({ className = '' }) => {
+  const [show, setShow] = useState(true)
+  const [lastScrollY, setLastScrollY] = useState(0)
+
+  const controlNavbar = () => {
+    if (typeof window !== 'undefined') {
+      if (window.scrollY > lastScrollY) {
+        // if scroll up show the navbar
+        setShow(true)
+      } else {
+        // if scroll down hide the navbar
+        setShow(false)
+      }
+
+      // remember current page location to use in the next move
+      setLastScrollY(window.scrollY)
+    }
+  }
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', controlNavbar)
+
+      // cleanup function
+      return () => {
+        window.removeEventListener('scroll', controlNavbar)
+      }
+    }
+  }, [lastScrollY])
+
   return (
-    <header className={classNames(styles.header, className)}>
+    <header className={classNames(styles.header, styles[`${show && 'hidden'}`], className)}>
       <div className={styles.inner}>
         <Link href="/" passHref className={styles.logo}>
           <a className={styles.logo}>
