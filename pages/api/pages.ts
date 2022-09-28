f6a5ea2d30cd0e4d57ba62ce18effe2687e79fc0
Dave Williams <mail@d4v3w.com>
2022-09-28 01:03:14 +0100

    update api

---
+++ pages/api/pages.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { newsData as pages } from '../../utils/news-items'
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { type, id } = req.query
+  try {
+    let result
+    if (id) {
+      result = Object.values(pages).filter((v) => v.id.toString() === id)
+    } else if (type) {
+      result = Object.values(pages).filter((v) => v.type.startsWith(`${type}`))
+    }
+    if (!result) {
+      throw new Error('Invalid request. Try another query.')
+    }
+    res.status(200).json(result)
+  } catch (error) {
+    const message = error instanceof Error ? error.message : String(error)
+    res.status(400).json({ statusCode: 400, message: message })
+  }
+}
+
+export default handler
