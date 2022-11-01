8905a9ae13d7b15289cd7ebbaa0ee587918c2e34
Dave Williams <mail@d4v3w.com>
2022-11-01 00:46:53 +0000

    use legacy image

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames'
-import Image from 'next/image'
+import Image from 'next/legacy/image'
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import logoImage from '../public/images/logo/digital-native-logo-full.png'
--- components/ImageBox.tsx
+++ components/ImageBox.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames'
-import Image from 'next/image'
+import Image from 'next/legacy/image'
 import styles from './imageBox.module.css'
 
 const ImageBox = ({ width = 500, height = 500, alt = '', isBlock = false, ...props }) => {
