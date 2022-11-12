1b21cb22686bfd9ea6de0b8698644e8f402de431
Dave Williams <mail@d4v3w.com>
2022-11-12 14:54:44 +0000

    contentful api integration starter

--- utils/Config.ts
+++ utils/Config.ts
@@ -1,5 +1,5 @@
 export const Config = {
   pagination: {
-    pageSize: 2,
+    pageSize: 10,
   },
 }
--- utils/ContentfulApi.ts
+++ utils/ContentfulApi.ts
@@ -1,5 +1,7 @@
+import { Config } from './Config'
+
 export default class ContentfulApi {
-  static async callContentful(query: any) {
+  static async callContentful(query: string) {
     const fetchUrl = `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`
 
     const fetchOptions = {
@@ -18,4 +20,74 @@ export default class ContentfulApi {
       throw new Error('Could not fetch data from Contentful!')
     }
   }
+
+  static async getTotalPostsNumber(type = 'news') {
+    const query = `
+      {
+        contentTypeCollection(
+          where: { name_in: "${type}" }
+        ) {
+          items {
+            name
+            linkedFrom {
+              contentCollection {
+                total
+              }
+            }
+          }
+        }
+      }
+    `
+
+    // Call out to the API
+    const response = await this.callContentful(query)
+    const totalPosts = response.data.contentTypeCollection.total ? response.data.blogPostCollection.total : 0
+
+    return totalPosts
+  }
+
+  static async getPaginatedContent(type = 'news', page: number) {
+    const skipMultiplier = page === 1 ? 0 : page - 1
+    const skip = skipMultiplier > 0 ? Config.pagination.pageSize * skipMultiplier : 0
+
+    const query = `{
+        contentTypeCollection(
+          where: { name_in: "${type}" }
+          order: sys_publishedAt_DESC
+        ) {
+          items {
+            name
+            linkedFrom {
+              contentCollection(limit: ${Config.pagination.pageSize}, skip: ${skip}) {
+                total
+                items {
+                  sys {
+                    id
+                    publishedAt
+                  }
+                  title
+                  slug
+                  summary
+                  story
+                  image
+                  link
+                }
+              }
+            }
+          }
+        }
+      }`
+
+    // Call out to the API
+    const response = await this.callContentful(query)
+    return getData(response)
+  }
+}
+
+const getData = (response: {
+  data: { contentTypeCollection: { items: { linkedFrom: { contentCollection: any } }[] } }
+}) => {
+  return response.data.contentTypeCollection.items[0]
+    ? response.data.contentTypeCollection.items[0].linkedFrom.contentCollection
+    : { total: 0, items: [] }
 }
