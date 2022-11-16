c25f8b00a652798bebfa28e5a27a9dbc45254f65
Dave Williams <dave@fragile-media.co.uk>
2022-11-16 20:52:07 +0000

    Remove webpack config
    Try it and see

--- next.config.js
+++ next.config.js
@@ -13,23 +13,4 @@ module.exports = {
         locales: ["en"],
         defaultLocale: "en",
     },
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        // Note: we provide webpack above so you should not `require` it
-        // Perform customizations to webpack config
-        // Important: return the modified config
-
-        // [Webpack 5] Automatic Node.js Polyfills Removed
-        // https://github.com/webpack/webpack/issues/11282
-        // config.resolve.fallback = { util: require.resolve("webpack/lib/util/") };
-        // config.resolve.fallback = { fs: false };
-
-        // SVG support
-        config.module.rules.push({
-            test: /\.svg$/i,
-            issuer: /\.[jt]sx?$/,
-            use: ['@svgr/webpack'],
-        });
-
-        return config
-    },
 };
