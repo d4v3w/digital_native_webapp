8bfeee435c6bd54f79174d5e10d051c7d79a2511
Dave Williams <dave@fragile-media.co.uk>
2022-11-16 20:52:07 +0000

    Remove webpack config
    Try it and see

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    experimental: {
+        appDir: true,
+    },
+    reactStrictMode: true,
+    images: {
+        domains: ['www.digitalnativeuk.com', 'images.ctfassets.net'],
+    },
+    compiler: {
+        styledComponents: true,
+    },
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+    },
+};
